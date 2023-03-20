04c09c7ed8a97d93d72e9a987f49981de6989a96
Sess254 <wamaritemoses@gmail.com>
2023-03-20 19:17:13 +0300

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: Sess254
+ * Desc: Header file that defines a new type struct dog.
+ */
+
+/**
+ * struct dog - New type describing a dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
