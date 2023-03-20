6bb46b62b6c4d33daf28ac800aed5c118ead6095
Sess254 <wamaritemoses@gmail.com>
2023-03-20 16:15:46 +0300

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-main.c
+ * Auth: Sess254
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ * followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+
+	return (0);
+}
