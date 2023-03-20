12b2cb9c7affe3eb5c1b085fae5ac31160c5d678
Sess254 <wamaritemoses@gmail.com>
2023-03-20 15:55:01 +0300

    object like macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Sess254
+ * Desc: Heeder file that defines a macro named
+ * size as an abbreviation for the token 1024.
+ *
+ */
+
+#define SIZE 1024
+
+#endif
