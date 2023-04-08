4163967fd7180c7235f59743f053f661ae4451c4
Sess254 <wamaritemoses@gmail.com>
2023-04-08 13:24:01 +0300

    fixed some checkers

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
  */
 
 #include "main.h"
-#include <stddef.h>
+#include <stdio.h>
 
 /**
  *binary_to_uint - converts a binary number to
@@ -17,31 +17,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number;
+	unsigned int number = 0;
 	int i;
 
 	if (b == NULL)
 		return (0);
 
-
-	while (b[i] != '\0')
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == '0')
+		if (b[i] != '0' && b[i] != '1')
 		{
-			number = number << 1;
-		}
-
-		else if (b[i] == '1')
-		{
-			number = (number << 1) | 1;
-		}
-
-		else
 			return (0);
+		}
 
-		i++;
+		number = (number << 1) + (b[i] - '0');
 	}
 
 	return (number);
-
 }
