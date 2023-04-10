05f6d7e56cf8232028c3871c825df7194d51fa85
Sess254 <wamaritemoses@gmail.com>
2023-04-10 23:42:17 +0300

    minor adjustment to under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 			strln++;
 	}
 
-	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC | S_IRUSR | S_IWUSR);
+	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fw = write(fo, text_content, strln);
 
 	if (fo == -1 || fw == -1)
