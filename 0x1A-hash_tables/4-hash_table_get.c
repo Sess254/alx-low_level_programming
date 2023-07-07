48a0c72a6ce2e44463b802c2fe429e8c1e9d0375
Sess254 <wamaritemoses@gmail.com>
2023-07-07 16:06:18 +0300

    updated .h

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,7 +17,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int idx;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || *key == '\0' || key == NULL)
 		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
