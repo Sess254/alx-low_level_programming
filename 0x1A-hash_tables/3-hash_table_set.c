94c5923fad74fc0b066d31e8394eb4ccf96bad0e
Sess254 <wamaritemoses@gmail.com>
2023-07-07 14:24:26 +0300

    error correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,7 +43,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(val_cpy);
 		return (0);
 	}
-
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
 	new->value = val_cpy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
