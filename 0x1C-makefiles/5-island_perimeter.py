7d9478424c38641635a64fc24e8203abd00366d2
Sess254 <wamaritemoses@gmail.com>
2023-08-23 17:41:47 +0300

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter.py
+"""
+
+
+def island_perimeter(grid):
+    """ Calculates the perimeter of an island described in grid """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
